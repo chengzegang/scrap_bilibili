acc4c61d139504438561240d45bc5c36e104b39d
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2023-11-04 14:54:26 -0400

    update

--- dataset.py
+++ dataset.py
@@ -1,4 +1,12 @@
-from torch.utils.data import MapDataPipe, Dataset, IterableDataset, IterDataPipe
+from typing import Any
+from torch.utils.data import (
+    MapDataPipe,
+    Dataset,
+    IterableDataset,
+    IterDataPipe,
+    DataChunk,
+    functional_datapipe,
+)
 import datasets
 import zipfile
 import glob
@@ -11,73 +19,95 @@ import torchvision.transforms.v2.functional as TF
 import io
 import webdataset as wds
 import dropbox
+from upload import list_exists
+import torch.utils.data.datapipes as dp
 
 
-class MVFDataset(MapDataPipe, Dataset):
-    def __init__(self, root: str):
+@functional_datapipe("mvf_path")
+class _MVFPathPipe(IterDataPipe, IterableDataset):
+    def __init__(self, **kwargs):
         super().__init__()
-        self.root = root
-        paths = glob.glob(os.path.join(root, "*.zip"))
-        self.paths = np.asarray(paths)
+        self.dbx = dropbox.Dropbox(
+            oauth2_refresh_token=os.environ["DBX_REFRESH_TOKEN"],
+            oauth2_access_token=os.environ["DBX_ACCESS_TOKEN"],
+            app_key=os.environ["DBX_APP_KEY"],
+            app_secret=os.environ["DBX_APP_SECRET"],
+        )
+        self.filenames = np.asarray(list_exists(self.dbx, "/MVFdataset", ".zip"))
+
+    def __getitem__(self, index):
+        return self.filenames[index]
 
     def __len__(self):
-        return len(self.paths)
+        return len(self.filenames)
 
-    def load_data(self, path: str) -> Tensor:
-        imgs = []
-        with zipfile.ZipFile(path, "r") as f:
-            for name in sorted(f.namelist()):
-                img_bytes = f.read(name)
-                img = Image.open(io.BytesIO(img_bytes))
-                img = TF.pil_to_tensor(img)
-                imgs.append(img)
-        imgs = torch.stack(imgs)
-        return imgs
-
-    def __getitem__(self, index: int) -> Tensor:
-        path = self.paths[index]
-        imgs = self.load_data(path)
-        return imgs
+    def __iter__(self):
+        for filename in self.filenames:
+            yield filename
 
 
-class DBXdataset(MapDataPipe, Dataset):
-    def __init__(self, shared_link: str, **kwargs):
-        super().__init__(**kwargs)
+@functional_datapipe("dbx_download")
+class _DBXDownloader(IterDataPipe, IterableDataset):
+    def __init__(self, datapipe: IterDataPipe, **kwargs):
+        super().__init__()
+        self.datapipe = datapipe
         self.dbx = dropbox.Dropbox(
-            app_key=os.environ["DBX_APP_KEY"], app_secret=os.environ["DBX_APP_SECRET"]
+            oauth2_refresh_token=os.environ["DBX_REFRESH_TOKEN"],
+            oauth2_access_token=os.environ["DBX_ACCESS_TOKEN"],
+            app_key=os.environ["DBX_APP_KEY"],
+            app_secret=os.environ["DBX_APP_SECRET"],
         )
-        self.shared_link = shared_link
-        self.filenames = np.asarray(list(self.ls))
 
-    @property
-    def ls(self):
-        resp = self.dbx.files_list_folder(
-            path="", shared_link=dropbox.files.SharedLink(url=self.shared_link)
-        )
-        while True:
-            for entry in resp.entries:
-                yield entry.name
-            if resp.has_more:
-                resp = self.dbx.files_list_folder_continue(resp.cursor)
-            else:
-                break
+    def __iter__(self):
+        return len(self.datapipe)
 
     def download(self, remote_path: str):
-        resp = dropbox.Dropbox.sharing_get_shared_link_file(
-            url=self.shared_link, path="/" + remote_path
-        )
+        bv = os.path.basename(remote_path).split(".")[0]
+        meta, resp = self.dbx.files_download(os.path.join("/MVFdataset", remote_path))
         imgs = []
-        basename = os.path.basename(remote_path)
-        bv = os.path.splitext(basename)[0]
-        with zipfile.ZipFile(resp.content, "r") as zf:
+        with zipfile.ZipFile(io.BytesIO(resp.content), "r") as zf:
             for name in sorted(zf.namelist()):
-                if name.startswith(".jpg"):
+                if name.endswith(".jpg"):
                     img = Image.open(io.BytesIO(zf.read(name))).convert("RGB")
                     img = TF.pil_to_tensor(img)
                     imgs.append(img)
         imgs = torch.stack(imgs)
         return {"bv": bv, "frames": imgs}
 
-    def __getitem__(self, index: int) -> Tensor:
-        path = self.filenames[index]
-        return self.download(path)
+    def __iter__(self):
+        for path in self.datapipe:
+            yield self.download(path)
+
+    def __getitem__(self, index) -> Any:
+        return NotImplementedError
+
+
+@functional_datapipe("mvf_dataset")
+class MVFDataset(IterDataPipe, IterableDataset):
+    def __init__(self, image_size: int, transform=None, **kwargs):
+        super().__init__()
+        self._mvfpaths = _MVFPathPipe(**kwargs)
+        self.image_size = image_size
+        self.transform = transform
+        self.dp = (
+            dp.iter.IterableWrapper(self._mvfpaths)
+            .sharding_filter()
+            .shuffle()
+            .dbx_download()
+            .map(self.resize)
+            .map(transform if transform is not None else lambda x: x)
+        )
+
+    def resize(self, image: Tensor) -> Tensor:
+        image["frames"] = TF.resize(image["frames"], self.image_size, antialias=True)
+        return image
+
+    def __len__(self):
+        return len(self._mvfpaths)
+
+    def __iter__(self):
+        for data in self.dp:
+            yield data
+
+    def __getitem__(self, index) -> Any:
+        return NotImplementedError
